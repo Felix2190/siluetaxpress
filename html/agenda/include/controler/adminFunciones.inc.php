8bbf7f8532995debd7d6205d1e96de57012803d0
Felix2190 <felixortiz@planetcommunication.net>
2018-10-23 00:34:21 -0500

    max execution

--- html/agenda/include/controler/adminFunciones.inc.php
+++ html/agenda/include/controler/adminFunciones.inc.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('max_execution_time', 5000);
-ini_set('max_input_time', 5000);
+ini_set('max_execution_time', 300);
+ini_set('max_input_time', 300);
 
 if(!empty($_FILES['imagen']))
 {
