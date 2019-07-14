f364405f4b069400dc888b277b49c7f2a133aeed
Felix2190 <felixortiz@planetcommunication.net>
2019-07-14 16:01:15 -0500

    cambios

--- html/index.php
+++ html/index.php
@@ -1,16 +1,13 @@
 <?php 
 ?>
 <!DOCTYPE HTML>
-<!--
-	Minimaxing by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
 	<head>
-		<title>SiluetaExpress</title>
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		    <meta name="Description" CONTENT="Author: Lezlie, silueta express, SILUETA EXPRESSS">
+		<title>Silueta Express</title>
+		    <meta name="robots" content="index,follow">
+		    <meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
